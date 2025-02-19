d3c5628a796b9049bf3a7c0caa829a0fbda3dddf
Minami <minamikanesawa@gmail.com>
2025-02-19 12:26:49 +0100

    add 20250219_2

---
+++ ADT/20250219_2 /b/a.cpp
@@ -0,0 +1,12 @@
+/* 解説より
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int k;
+  cin >> k;
+  int h = 21 + k / 60, m = k % 60;
+  printf ("%02d:%02d\n", h, m);
+  return 0;
+}
+*/
