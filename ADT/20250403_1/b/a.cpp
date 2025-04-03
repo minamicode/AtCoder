e81442232519a2769b21eaab5155bef45225ef41
Minami <minamikanesawa@gmail.com>
2025-04-03 13:28:59 +0200

    add 20250403_1

---
+++ ADT/20250403_1/b/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  
+  char a = '0';
+  char b = s.at(0);
+  char c = s.at(1);
+  char d = s.at(2);
+  
+  cout << a << b << c << d << endl;
+}
