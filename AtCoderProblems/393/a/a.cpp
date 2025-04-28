15e1dc3ffe2a1537442f68f36555de33ba6d3d59
Minami <minamikanesawa@gmail.com>
2025-04-28 19:04:52 +0200

    add 393a

---
+++ AtCoderProblems/393/a/a.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s1, s2;
+  cin >> s1 >> s2;
+  
+  if (s1 == "sick" && s2 == "sick") {
+    cout << 1 << endl;
+  } else if (s1 == "fine" && s2 == "sick") {
+    cout << 3 << endl;
+  } else if (s1 == "sick" && s2 == "fine") {
+    cout << 2 << endl;
+  } else if (s1 == "fine" && s2 == "fine") {
+    cout << 4 << endl;
+  }
+}
+
+/* 解説より
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s1, s2;
+  cin >> s1 >> s2;
+  if (s1 == "sick" && s2 == "sick") cout << 1 << endl;
+  if (s1 == "sick" && s2 == "fine") cout << 2 << endl;
+  if (s1 == "fine" && s2 == "sick") cout << 3 << endl;
+  if (s1 == "fine" && s2 == "fine") cout << 4 << endl;
+  return 0;
+}
+*/
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s1, s2;
+  cin >> s1 >> s2;
+  int ans = 1;
+  if (s1 == "fine") ans += 2;
+  if (s2 == "fine") ans += 1;
+  cout << ans << endl;
+  return 0;
+}
+*/
