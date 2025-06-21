ed7b1d1734e05e6510218f1aaf5e5c0fa9d4a92c
Minami <minamikanesawa@gmail.com>
2025-06-21 16:31:49 +0200

    add ABC411

---
+++ ABC/411/a/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string p;
+    cin >> p;
+    int l;
+    cin >> l;
+
+    if (p.size() >= l) {
+      cout << "Yes" << endl;
+    } else {
+      cout << "No" << endl;
+    } 
+    
+    return 0;
+}
