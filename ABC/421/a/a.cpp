a6ada3f467dcac4d50c6f00e35e7490927de10d2
Minami <minamikanesawa@gmail.com>
2025-08-30 17:43:42 +0200

    add ABC421

---
+++ ABC/421/a/a.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int n;
+  cin >> n;
+
+  vector<string> s(n);
+  for (int i = 0; i < n; i++) {
+    cin >> s.at(i);
+  }
+
+  int x;
+  cin >> x;
+  int z = x - 1;
+
+  string y;
+  cin >> y;
+
+  bool ok = false;
+
+  for (int i = 0; i < n; i++) {
+    if (z == i && y == s.at(i)) {
+      ok = true;
+      break;
+    } else {
+      ok = false;
+    }
+  }
+
+  if (ok) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  
+  return 0;
+}
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<string> s(n+1);
+  for (int i = 1; i <= n; i++) {
+    cin >> s[i];
+  }
+
+  int x; string y;
+  cin >> x >> y;
+  if (s[x] == y) cout << "Yes" << endl;
+  else cout << "No" << endl;
+  return 0;
+}
+*/
